411fcdd4176fd253617d238e5e2d660168b8c9f3
mariyamcodehub <mariyamcodehub@users.noreply.github.com>
2024-03-25 01:28:10 +0530

    adding styles to login page

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-full h-screen bg-current place-content-center'>
+      <div className='w-2/5 h-4/5 bg-gray-50 text-black ml-auto mr-auto'>
+        <h1 className='text-slate-400 text-3xl text-center'>BICO</h1>
+        <h2 className='text-3xl font-bold' >Login to your account</h2>
+      </div>
+
+    </div>
   )
 }
 
