fb24712452c3ff6660be49975a0b32cd483aef2e
Ayush6196 <ayushshukla6196@gmail.com>
2024-04-04 10:46:41 +0530

    change

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -6,13 +6,12 @@ const Login = () => {
     <>
 
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-baseline lg:flex-left ">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold ms-8">Login now!</h1>
-            <p className="py-8 px-8">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form className="card-body ">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
