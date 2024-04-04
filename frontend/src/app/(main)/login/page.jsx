7e9fe0673c94cabcce2e978798e4341599d5e727
mariyamcodehub <mariyamcodehub@users.noreply.github.com>
2024-04-04 10:47:43 +0530

    change background

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -5,12 +5,10 @@ const Login = () => {
   return (
     <>
 
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold ms-8">Login now!</h1>
-            <p className="py-8 px-8">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
+      <div className="hero min-h-screen bg-blend-luminosity">
+
+        <div className="hero-content flex-col ">
+          <h1 className="text-5xl font-bold ms-8 text-white">Login now!</h1>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form className="card-body">
               <div className="form-control">
