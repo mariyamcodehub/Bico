03d91a5ad9c75c23e54d8a67192a76127b2c5b5f
mariyamcodehub <mariyamcodehub@users.noreply.github.com>
2024-04-06 11:30:01 +0530

    new edit

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="collapse bg-base-200">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Who we are?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aut, iusto possimus natus facilis molestias voluptatibus ex! Debitis blanditiis illum, in fugiat consequatur magni unde tenetur, aperiam sint quas eveniet distinctio adipisci magnam laboriosam temporibus dignissimos corrupti, quia expedita obcaecati rem sapiente ad dolore? Labore delectus eos totam porro repudiandae error iure doloremque dolores itaque. Velit quaerat eum voluptatem molestiae, provident, nulla, id tempora voluptatibus aut exercitationem minus! Ipsa in dolor, eaque maiores adipisci enim sed odio laborum, perferendis dolores optio. Voluptatem sequi itaque placeat assumenda consequatur, expedita quo nemo neque. Saepe vel blanditiis sint repudiandae officiis nulla fugiat hic?</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
