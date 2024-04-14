788cba7c4f0a6dd5f039b0730c5e176014d5f7ba
Ayush6196 <ayushshukla6196@gmail.com>
2024-04-14 12:31:47 +0530

    new

--- frontend/src/app/(main)/campaignForm/page.jsx
+++ frontend/src/app/(main)/campaignForm/page.jsx
@@ -19,11 +19,11 @@ const campaignForm = ({ coverImage, coverTitle, coverDescription }) => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 img">
+        <div className="flex items-center justify-end min-h-screen bg-gray-100 img">
 
-            <div className={`transition-opacity duration-700 ease-out ${isMounted ? 'opacity-100' : 'opacity-0'}`}>
-                <div className="p-8 transition-all transform bg-white shadow-xl rounded-lg scale-95 hover:scale-100">
-                    <h2 className="text-2xl font-semibold text-center text-indigo-600">Add Campaign</h2>
+            <div className={`transition-opacity duration-700 ease-out ${isMounted ? 'opacity-100' : 'opacity-50'}`}>
+                <div className="p-8 mr-48 transition-all transform bg-base shadow-xl rounded-lg scale-95 hover:scale-100">
+                    <h2 className="text-2xl font-semibold text-center text">Add Campaign</h2>
                     <Formik
                         initialValues={{ email: '', password: '' }}
                         onSubmit={handleSubmit}
--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -33,13 +33,13 @@ const Signup = () => {
   return (
     <>
       <div className="hero min-h-screen md:justify-end ">
-        <div className="hero-content mr-20 w-full">
+        <div className="hero-content mr-48 w-full">
 
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-stone-700 opacity-75 hover:opacity-100  ">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-slate-200 opacity-75 hover:opacity-100  ">
             <form onSubmit={signupForm.handleSubmit} className="card-body">
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Name</span>
+                  <span className="label-text ">Name</span>
                 </label>
                 <input type="name" placeholder="Name" id='name' className="input input-bordered" onChange={signupForm.handleChange} value={signupForm.values.name} />
                 {
@@ -50,7 +50,7 @@ const Signup = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Email</span>
+                  <span className="label-text ">Email</span>
                 </label>
                 <input type="email" placeholder="email" className="input input-bordered" id="email"
                   onChange={signupForm.handleChange}
@@ -62,7 +62,7 @@ const Signup = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text text-white">Create Password</span>
+                  <span className="label-text ">Create Password</span>
                 </label>
                 <input type="password" placeholder="Create Password" className="input input-bordered" id="password"
                   onChange={signupForm.handleChange}
@@ -75,7 +75,7 @@ const Signup = () => {
               <div className="form-control">
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text text-white">Confirm Password</span>
+                    <span className="label-text ">Confirm Password</span>
                   </label>
                   <input type="cpassword" placeholder="Confirm password" className="input input-bordered" id='cpassword' onChange={signupForm.handleChange}
                     value={signupForm.values.cpassword} required />
@@ -86,7 +86,7 @@ const Signup = () => {
                 </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text text-white">Social-Link</span>
+                    <span className="label-text ">Social-Link</span>
                   </label>
                   <input type="social" placeholder="Social-Link" className="input input-bordered" id='social' required />
                 </div>
